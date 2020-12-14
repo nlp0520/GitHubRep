10f5227274daa9d89dbe359da3243795380e8258
niulipeng <784115501@qq.com>
2020-12-14 14:35:56 +0800

    idea破解

---
+++ src/com/niulipeng/IDEA.java
@@ -0,0 +1,10 @@
+package com.niulipeng;
+
+/**
+ * 破解IDEA
+ * 将安装包解压并将jetbrains-agent.jar包放到安装包内
+ * 打开IDEA选择免费版进入新建项目不导入配置
+ * HELP->rigister->license server->https://fls.jetbrains-agent.com
+ */
+public class IDEA {
+}
